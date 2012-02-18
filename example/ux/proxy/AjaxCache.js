6fb7e78af4ad18be9d978414a929b7ad0545642b
Adam Duncan <adam.duncan@clock.co.uk>
2012-02-18 13:11:35 +0000

    Updated example

--- example/ux/proxy/AjaxCache.js
+++ example/ux/proxy/AjaxCache.js
@@ -1,21 +1,59 @@
+/**
+ * @class Ext.ux.proxy.AjaxCache
+ * @extend Ext.data.proxy.Ajax
+ *
+ * An extension of the {Ext.data.proxy.Ajax} class which adds caching capabilities
+ * @notes Use this class as an example if you want to implement {@link Ext.ux.proxy.ProxyCache} in your own custom proxy
+ * @author Adam Duncan
+*/
 Ext.define('Ext.ux.proxy.AjaxCache', {
 	extend: 'Ext.data.proxy.Ajax',
 	alias: 'proxy.ajaxCache',
+
+	/**
+	* Mixin the functions from {@link Ext.ux.proxy.ProxyCache}
+	*/
 	mixins: {
 		proxyCache: 'Ext.ux.proxy.ProxyCache'
 	},
 
+	/**
+	*	Override the constructor so that we can get access to the proxy config
+	* Make sure the parent constructor is called before setCacheConfig
+	*
+	* @param {Ext.Object.classify.config} Class config
+	*/
 	constructor: function(config) {
 		this.callParent(arguments);
 		this.setCacheConfig(this.config);
 	},
 
+	/**
+	 * Override the read function so that we can check if the response is already cached and return it from their instead of going to the server.
+	 * This function must be included in any custom proxies that wish to implement {@link Ext.ux.proxy.ProxyCache}
+	 *
+	 * @param {Ext.data.Operation} operation The operation being executed
+	 * @param {function} callback Callback to be executed when operation has completed
+	 * @param {Object} scope Scope for the callback function
+	 */
 	read: function(operation, callback, scope) {
 		if (!this.inCache(operation, callback, scope)) {
 			this.callParent(arguments);
 		} 
 	},
 
+	/**
+	 * Override the processResponse function so that we can the response to the cache after we have recieved it from the server.
+	 * This function must be included in any custom proxies that wish to implement {@link Ext.ux.proxy.ProxyCache}
+	 * Make sure addToCache is called before the parent function
+	 *
+	 * @param {Boolean} success Whether the operation was successful or not
+	 * @param {Ext.data.Operation} operation The operation being executed
+	 * @param {Ext.data.Request} request Request being sent to the server
+	 * @param {Ext.data.Response} response Response returned from the server
+	 * @param {function} callback Callback to be executed when operation has completed
+	 * @param {Object} scope Scope for the callback function
+	 */
 	processResponse: function(success, operation, request, response, callback, scope) {
 		this.addToCache(request, response);
 		return this.callParent(arguments);
