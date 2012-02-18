d81cd0cd23b3631840a8ad6ebb1d921fa3f5fc65
Adam Duncan <adam.duncan@clock.co.uk>
2012-02-18 13:09:51 +0000

    Added comments

--- ux/proxy/ProxyCache.js
+++ ux/proxy/ProxyCache.js
@@ -1,10 +1,30 @@
+/**
+* @class Ext.ux.proxy.ProxyCache
+*
+* A dual storage remote caching proxy. Caches the responses from remote requests in HTML5 Local Storage in order to reduce the number of HTTP requests that need to be made.
+*	This class should never be used directly. Should be instantiated via mixins in another class which extends {@link Ext.data.proxy.Server}
+*
+* @author Adam Duncan
+*/
 Ext.define('Ext.ux.proxy.ProxyCache', {
+	/**
+	* Current in-memory cache
+	*/
 	cache: {},
+
+	/**
+	*	Default cache values
+	*/
 	configDefaults: {
 		cacheTimeout: 3600,
 		cacheKey: 'proxyCache'
 	},
 
+	/**
+	 * Sets the default cache values if they have not been defined and determines if they are valid.
+	 *
+	 * @param {Ext.Object.classify.config} Class config
+	 */
 	setCacheConfig: function(config) {
 		if (!config.cacheTimeout) {
 			this.config.cacheTimeout = this.configDefaults.cacheTimeout;
@@ -17,6 +37,9 @@ Ext.define('Ext.ux.proxy.ProxyCache', {
 		}
 	},
 
+	/**
+	 * Clears cache entries which have passed their expiration time.
+	 */
 	runGarbageCollection: function() {
 		var now = Date.now();
 		for (var key in this.cache) {
@@ -27,6 +50,14 @@ Ext.define('Ext.ux.proxy.ProxyCache', {
 		window.localStorage.setItem(this.config.cacheKey, Ext.encode(this.cache));
 	},
 
+	/**
+	 * Determines if response is cached and processes the cached response if it is.
+	 *
+	 * @param {Ext.data.Operation} operation The operation being executed
+	 * @param {function} callback Callback to be executed when operation has completed
+	 * @param {Object} scope Scope for the callback function
+	 * @return {Boolean} if response is in cache
+	 */
 	inCache: function(operation, callback, scope) {
 		var request = this.buildRequest(operation, callback, scope);
 		var requestKey = this.config.url + Ext.encode(request.config.params);
@@ -47,6 +78,12 @@ Ext.define('Ext.ux.proxy.ProxyCache', {
 		}
 	},
 
+	/**
+	 * Adds the response to the cache if it does not already exist.
+	 *
+	 * @param {Ext.data.Request} request Request being sent to the server
+	 * @param {Ext.data.Response} response Response returned from the server
+	 */
 	addToCache: function(request, response) {
 		if (!response._cached) {
 			this.getCache();
@@ -68,6 +105,9 @@ Ext.define('Ext.ux.proxy.ProxyCache', {
 		}
 	},
 
+	/**
+	 * Loads the local storage cache into memory
+	 */
 	getCache: function() {
 		this.cache = window.localStorage.getItem(this.config.cacheKey);
 		if (this.cache === null) {
@@ -77,6 +117,12 @@ Ext.define('Ext.ux.proxy.ProxyCache', {
 		}
 	},
 
+	/**
+	 * Converts a string to an XML document
+	 *
+	 * @param {String} xml XML String
+	 * @return {Doc} Parsed XML document
+	 */
 	xmlToDocument: function(xml) {
 		var xmlParser = new DOMParser();
 		return xmlParser.parseFromString(xml, 'text/xml');
